2619a74f3f6c9198fe97972240b0d175c590a755
alexei <alexbayda01@gmail.com>
2023-12-25 22:07:14 +0200

    Added department_process html changed issue html and app py accordingly

--- code/app.py
+++ code/app.py
@@ -215,5 +215,16 @@ def issue():
         return redirect(url_for('login'))
 
 
+# Add this route to handle the "Підрозділу" section
+@app.route('/department_process')
+def department_process():
+    user = session.get('user')
+
+    if user:
+        return render_template('department_process.html', user=user)
+    else:
+        return redirect(url_for('login'))
+
+
 if __name__ == '__main__':
     app.run(debug=True)
