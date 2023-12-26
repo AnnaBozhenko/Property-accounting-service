91c57de7b756bc540f58150557f9e7bb30006fc0
alexei <alexbayda01@gmail.com>
2023-12-26 03:27:53 +0200

    Fixed problems on creating tables (relocated tables in right sequence, fixed 3L problem)

--- code/sql/sql.py
+++ code/sql/sql.py
@@ -26,23 +26,6 @@ CREATE TABLE IF NOT EXISTS users (
 cursor.execute(create_table_query)
 conn.commit()
 
-# Add this table creation query to the existing create_users_table function
-create_messages_table_query = '''
-CREATE TABLE IF NOT EXISTS messages (
-    id SERIAL PRIMARY KEY,
-    sender_id INT REFERENCES users(id),
-    recipient_id INT REFERENCES users(id),
-    subject VARCHAR(255) NOT NULL,
-    body TEXT,
-    sent_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    type VARCHAR(255) NOT NULL,
-    invoice_number INT REFERENCES delivery_notes(id),
-    folllow_letter_num INT REFERENCES folllow_letter(id)
-);
-'''
-cursor.execute(create_messages_table_query)
-conn.commit()
-
 # Function to create the records table if not exists
 create_entry_records_table_query = '''
 CREATE TABLE IF NOT EXISTS entry_records (
@@ -61,7 +44,7 @@ CREATE TABLE IF NOT EXISTS entry_records (
 cursor.execute(create_entry_records_table_query)
 conn.commit()
 create_delivery_notes_table_query = '''
-CREATE TABLE delivery_notes (
+CREATE TABLE IF NOT EXISTS delivery_notes (
     id SERIAL PRIMARY KEY,
     date_valid_until DATE,
     invoice_number VARCHAR(255),
@@ -87,7 +70,7 @@ cursor.execute(create_delivery_notes_table_query)
 conn.commit()
 
 create_follow_letter_table_query = '''
-CREATE TABLE follow_letter (
+CREATE TABLE IF NOT EXISTS follow_letter (
     id SERIAL PRIMARY KEY,
     military_property_name VARCHAR(255) NOT NULL,
     sender VARCHAR(255) NOT NULL,
@@ -100,4 +83,21 @@ CREATE TABLE follow_letter (
 );
 '''
 cursor.execute(create_follow_letter_table_query)
+conn.commit()
+
+# Add this table creation query to the existing create_users_table function
+create_messages_table_query = '''
+CREATE TABLE IF NOT EXISTS messages (
+    id SERIAL PRIMARY KEY,
+    sender_id INT REFERENCES users(id),
+    recipient_id INT REFERENCES users(id),
+    subject VARCHAR(255) NOT NULL,
+    body TEXT,
+    sent_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    type VARCHAR(255) NOT NULL,
+    invoice_number INT REFERENCES delivery_notes(id),
+    folllow_letter_num INT REFERENCES follow_letter(id)
+);
+'''
+cursor.execute(create_messages_table_query)
 conn.commit()
